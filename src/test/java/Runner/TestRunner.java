6e988982c03d9a41e345ccae09c2a5ac5d421b16
Prince <prnbtr@gmail.com>
2020-06-02 11:56:00 +0530

    Test runner changes for report

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -4,13 +4,12 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
 //		/"src/test/resources/Features",
 features= {"src/test/resources/ParallelFeatures"},
 glue= {"stepDefinitions","hooks"},
-plugin= {"junit:target/cucumber.xml" }, strict=false, tags= {"@Login"}
+plugin= {"json:target/cucumber.json" }, strict=false, tags= {"@Login"}
 )
 public class TestRunner {
 	
